ad64bf9d0eb76ef6c4305f541558bece3ba95292
Moshfiqur Rahman <studentemam@gmail.com>
2025-02-06 14:24:53 +0600

    feat: add Accordion component

--- src/practice/AccordionComponent.tsx
+++ src/practice/AccordionComponent.tsx
@@ -1,7 +1,33 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 export default function AccordionComponent() {
   return (
-    <div>
-      <p>Accordion</p>
+    <div className="w-[30%] mx-auto my-10">
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>What is the refund policy?</AccordionTrigger>
+          <AccordionContent>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque
+            recusandae molestiae tempore magnam adipisci dolorum, vero maiores,
+            velit corporis enim incidunt laborum rerum. Incidunt cumque numquam
+            ea perferendis voluptatibus? Sint.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>What is the payment process?</AccordionTrigger>
+          <AccordionContent>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque
+            recusandae molestiae tempore magnam adipisci dolorum, vero maiores,
+            velit corporis enim incidunt laborum rerum. Incidunt cumque numquam
+            ea perferendis voluptatibus? Sint.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
