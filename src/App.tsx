fd02f3ef489717bbbb2872bd36f811bb92be0d0b
Moshfiqur Rahman <studentemam@gmail.com>
2025-02-06 14:14:05 +0600

    init: initialize project setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Fragment } from "react/jsx-runtime";
+import AccordionComponent from "./practice/AccordionComponent";
+
+export default function App() {
+  return (
+    <Fragment>
+      <AccordionComponent />
+    </Fragment>
+  );
+}
---
+++ src/practice/AccordionComponent.tsx
@@ -0,0 +1,7 @@
+export default function AccordionComponent() {
+  return (
+    <div>
+      <p>Accordion</p>
+    </div>
+  );
+}
